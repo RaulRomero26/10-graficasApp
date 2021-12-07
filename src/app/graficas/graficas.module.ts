c2c6b595b99eb6afbea520110576d96b75172190
Raul <link_2642@hotmail.com>
2021-12-06 20:40:56 -0600

    ng2 chart version vieja funcional

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { NgChartsModule } from 'ng2-charts';
+import { ChartsModule } from 'ng2-charts';
 
 import { GraficasRoutingModule } from './graficas-routing.module';
 import { BarrasComponent } from './pages/barras/barras.component';
@@ -22,7 +22,7 @@ import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.
   imports: [
     CommonModule,
     GraficasRoutingModule,
-    NgChartsModule
+    ChartsModule
   ]
 })
 export class GraficasModule { }
--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
+import { Component, OnInit, } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 
 
@@ -12,48 +12,36 @@ import { BaseChartDirective } from 'ng2-charts';
 })
 export class BarrasComponent implements OnInit {
 
-  public barChartOptions: ChartConfiguration['options'] = {
+  public barChartOptions: ChartOptions = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
     scales: {
-      x: {},
-      y: {
-        min: 10
-      }
+      xAxes:[{}],
+      yAxes: [{}]
     },
     plugins: {
-      legend: {
-        display: true,
-      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end'
+      }
     }
   };
+  public barChartLabels: Label[] = [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ];
   public barChartType: ChartType = 'bar';
-
-  public barChartData: ChartData<'bar'> = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
-    datasets: [
+  public barChartLegend = true;
+  public barChartData: ChartDataSets[] = [
       { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
       { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
     ]
-  };
-
-  // events
-  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
-
-  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
-
   
-  constructor() { 
-    
+  constructor() {  }
+  
+  ngOnInit(): void {
   }
 
   public randomize(): void {
     // Only Change 3 values
-    this.barChartData.datasets[0].data = [
+    this.barChartData[0].data = [
       Math.round(Math.random() * 100),
       59,
       80,
@@ -62,11 +50,6 @@ export class BarrasComponent implements OnInit {
       Math.round(Math.random() * 100),
       40 ];
 
-    this.chart?.update();
-  }
-
-  
-  ngOnInit(): void {
   }
 
 }
