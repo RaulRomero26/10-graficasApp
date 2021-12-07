f9f98fb9544326933d2751ce723a06f6d325e83f
Raul <link_2642@hotmail.com>
2021-12-06 21:47:37 -0600

    componete grafica dooble

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -8,8 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficaBarraComponent implements OnInit {
 
-  constructor() { }
-
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+  };
+  public barChartLabels: Label[] = [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData: ChartDataSets[] = [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#F6775D', hoverBackgroundColor: 'red'},
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#D45350', hoverBackgroundColor: 'red'},
+      { data: [ 8, 38, 70, 59, 66, 80, 100 ], label: 'Series C', backgroundColor: '#EB659F', hoverBackgroundColor: 'red'},
+    ]
+  
+  constructor() {  }
+  
   ngOnInit(): void {
   }
 
--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -15,23 +15,14 @@ export class BarrasComponent implements OnInit {
   public barChartOptions: ChartOptions = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      xAxes:[{}],
-      yAxes: [{}]
-    },
-    plugins: {
-      datalabels: {
-        anchor: 'end',
-        align: 'end'
-      }
-    }
   };
   public barChartLabels: Label[] = [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartData: ChartDataSets[] = [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#F6775D', hoverBackgroundColor: 'red'},
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#D45350', hoverBackgroundColor: 'red'},
+      { data: [ 8, 38, 70, 59, 66, 80, 100 ], label: 'Series C', backgroundColor: '#EB659F', hoverBackgroundColor: 'red'},
     ]
   
   constructor() {  }
@@ -43,12 +34,30 @@ export class BarrasComponent implements OnInit {
     // Only Change 3 values
     this.barChartData[0].data = [
       Math.round(Math.random() * 100),
-      59,
-      80,
       Math.round(Math.random() * 100),
-      56,
       Math.round(Math.random() * 100),
-      40 ];
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100), ];
+
+    this.barChartData[1].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100), ]; 
+      
+    this.barChartData[2].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100), ]; 
 
   }
 
